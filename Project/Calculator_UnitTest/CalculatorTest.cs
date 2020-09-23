27c65045b4ededf61bfaf2f819d0e43426b30195
VaniPotla <71787141+VaniPotla@users.noreply.github.com>
2020-09-24 01:38:30 +0530

    Added Calculator Unit Test Cases

---
+++ Project/Calculator_UnitTest/CalculatorTest.cs
@@ -0,0 +1,56 @@
+﻿using Calculator_Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace Calculator_UnitTest
+{
+  public  class CalculatorTest
+    {
+        
+        private readonly CalculatorAdapter _calculatorAdapter;
+        public CalculatorTest()
+        {
+            _calculatorAdapter = new CalculatorAdapter();
+        }
+
+        [Fact]
+        public void Add_ShouldReturnResult()
+        {
+            int start = 4;
+            int amount = 2;
+            int result = _calculatorAdapter.Add(start, amount);
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Subtract_ShouldReturnResult()
+        {
+            int start = 4;
+            int amount = 2;
+            int result = _calculatorAdapter.Subtract(start, amount);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Multiply_ShouldReturnResult()
+        {
+            int start = 4;
+            int by = 2;
+            int result = _calculatorAdapter.Multiply(start, by);
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void Divide_ShouldReturnResult()
+        {
+            int start = 4;
+            int by = 2;
+            int result = _calculatorAdapter.Divide(start, by);
+            Assert.Equal(2, result);
+        }
+    }
+}
+
