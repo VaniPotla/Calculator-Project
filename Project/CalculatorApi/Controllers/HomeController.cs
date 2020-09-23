4c875c39d00c817bee9997bfaea2d62b912df08c
VaniPotla <71787141+VaniPotla@users.noreply.github.com>
2020-09-24 02:18:35 +0530

    Added web api controller and Data access layer

---
+++ Project/CalculatorApi/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using CalculatorApi.DAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace CalculatorApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private readonly bool _isEntityFramework = false;
+        private DataAccessLayer _dataAccessLayer;
+
+        [Route("api/Home/Add")]
+        [HttpGet]
+        public int Add(int start, int amount)
+        {
+            int result = 0;
+            //Call EntityFrmework or DataAccessLayer For Ado.net based on bool value
+            if (_isEntityFramework)
+            {
+                using (var context = new CalculateEntities())
+                {
+                    object[] clientIdParameter = { new SqlParameter("@start", start),
+                        new SqlParameter("@amount", amount)
+                    };
+
+                    result = context.Database.ExecuteSqlCommand("exec SP_Add @start, @amount", clientIdParameter);
+                }
+            }
+            else
+            {
+                _dataAccessLayer = new DataAccessLayer();
+                result = _dataAccessLayer.Add(start, amount);
+            }
+            return result;
+        }
+
+        [Route("api/Home/Subtract")]
+        [HttpGet]
+        public int Subtract(int start, int amount)
+        {
+            int result = 0;
+            //Call EntityFrmework or DataAccessLayer For Ado.net based on bool value
+            if (_isEntityFramework)
+            {
+                using (var context = new CalculateEntities())
+                {
+                    object[] clientIdParameter = { new SqlParameter("@start", start),
+                        new SqlParameter("@amount", amount)
+                    };
+
+                    result = context.Database.ExecuteSqlCommand("exec SP_Subtract @start, @amount", clientIdParameter);
+                }
+            }
+            else
+            {
+                _dataAccessLayer = new DataAccessLayer();
+                result = _dataAccessLayer.Subtract(start, amount);
+            }
+            return result;
+        }
+
+        [Route("api/Home/Multiply")]
+        [HttpGet]
+        public int Multiply(int start, int by)
+        {
+            int result = 0;
+            //Call EntityFrmework or DataAccessLayer For Ado.net based on bool value
+            if (_isEntityFramework)
+            {
+                using (var context = new CalculateEntities())
+                {
+                    object[] clientIdParameter = { new SqlParameter("@start", start),
+                        new SqlParameter("@amount", by)
+                    };
+
+                    result = context.Database.ExecuteSqlCommand("exec SP_Multiply @start, @amount", clientIdParameter);
+                }
+            }
+            else
+            {
+                _dataAccessLayer = new DataAccessLayer();
+                result = _dataAccessLayer.Multiply(start, by);
+            }
+            return result;
+        }
+
+        [Route("api/Home/Divide")]
+        [HttpGet]
+        public int Divide(int start, int by)
+        {
+            int result = 0;
+            //Call EntityFrmework or DataAccessLayer For Ado.net based on bool value
+            if (_isEntityFramework)
+            {
+                using (var context = new CalculateEntities())
+                {
+                    object[] clientIdParameter = { new SqlParameter("@start", start),
+                        new SqlParameter("@amount", by)
+                    };
+
+                    result = context.Database.ExecuteSqlCommand("exec SP_Divide @start, @amount", clientIdParameter);
+                }
+            }
+            else
+            {
+                _dataAccessLayer = new DataAccessLayer();
+                result = _dataAccessLayer.Divide(start, by);
+            }
+            return result;
+        }
+    }
+}
---
+++ Project/CalculatorApi/DAL/DataAccessLayer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace CalculatorApi.DAL
+{
+    public class DataAccessLayer
+    {
+        private string _connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ToString();
+        public int Add(int start, int amount)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_Add", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@start", start);
+                    command.Parameters.AddWithValue("@amount", amount);
+                    connection.Open();
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+        }
+        public int Subtract(int start, int amount)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_Subtract", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@start", start);
+                    command.Parameters.AddWithValue("@amount", amount);
+                    connection.Open();
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+        }
+        public int Multiply(int start, int by)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_Multiply", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@start", start);
+                    command.Parameters.AddWithValue("@by", by);
+                    connection.Open();
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+        }
+        public int Divide(int start, int by)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_Divide", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@start", start);
+                    command.Parameters.AddWithValue("@by", by);
+                    connection.Open();
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+        }
+    }
+}
